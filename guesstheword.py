a1193a4184e74f9c95faba60faf4a00eeea7dd74
wallenc0de <wallencode@protonmail.com>
2018-12-30 13:42:51 +0100

    Update guesstheword.py

--- guesstheword.py
+++ guesstheword.py
@@ -1,3 +1,11 @@
+"""
+ ██████╗ ██╗   ██╗███████╗███████╗███████╗    ████████╗██╗  ██╗███████╗    ██╗    ██╗ ██████╗ ██████╗ ██████╗ 
+██╔════╝ ██║   ██║██╔════╝██╔════╝██╔════╝    ╚══██╔══╝██║  ██║██╔════╝    ██║    ██║██╔═══██╗██╔══██╗██╔══██╗
+██║  ███╗██║   ██║█████╗  ███████╗███████╗       ██║   ███████║█████╗      ██║ █╗ ██║██║   ██║██████╔╝██║  ██║
+██║   ██║██║   ██║██╔══╝  ╚════██║╚════██║       ██║   ██╔══██║██╔══╝      ██║███╗██║██║   ██║██╔══██╗██║  ██║
+╚██████╔╝╚██████╔╝███████╗███████║███████║       ██║   ██║  ██║███████╗    ╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝
+ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚══════╝     ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+"""
 # Simple 'Guess The Word' game that can be used for practice purpose]
 
 Questions = [
@@ -6,49 +14,30 @@ Questions = [
     ["3) What is the name of the company that makes operational systems in C++?: ", "Microsoft"]
 ]
 
-a = 0
 
 def y_n():
     while True:
         yes_no = input('Are you ready? (Y/N): ')
         if yes_no == 'Y':
             print('\n')
-            start_game(a)
-            break;
+            start_game()
+            break
         elif yes_no == 'N':
             print('Okay, ending the program.')
-            break;
+            break
         else:
             print('Error.')
 
-def start_game(a):
-    while a <= 2:
-        if a == 0:
-            word = input(Questions[0][0])
-            if word == Questions[0][1]:
-                print('You guessed the word. The word was: ' + Questions[0][1] + '.\n')
-                a += 1
+def start_game():
+    for question, answer in Questions:
+            word = input(question)
+            if word == answer:
+                print('You guessed the word.\n')
             else:
-                end_game()
-                break;
+                print('You did not guess the word.\nGAME OVER')
+                break
+    print('\nYou won the game!!')        
 
-        if a == 1:
-            word = input(Questions[1][0])
-            if word == Questions[1][1]:
-                print('You guessed the word. The word was: ' + Questions[1][1] + '.\n')
-                a += 1
-            else:
-                end_game()
-                break;
-
-        if a == 2:
-            word = input(Questions[2][0])
-            if word == Questions[2][1]:
-                print('You guessed the word. The word was: ' + Questions[2][1] + '.\n\nYOU WON THE GAME.')
-                break;
-            else:
-                end_game()
-                break;
 
 def end_game():
     print('You did not guess the word. Game Over.')
